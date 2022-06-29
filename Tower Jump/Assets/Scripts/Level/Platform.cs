ff6c86ceda54fdedce292276d4b15abd5a63f2f0
DangubaGitHub <dangubicjovan@outlook.com>
2022-06-29 10:33:56 +0200

    added more sprites and UI elements

--- Tower Jump/Assets/Scripts/Level/Platform.cs
+++ Tower Jump/Assets/Scripts/Level/Platform.cs
@@ -4,17 +4,6 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
-
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Platform Destroyer"))
