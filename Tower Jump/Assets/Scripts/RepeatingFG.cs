1f5e6cd23337e15ba8080b8c7b22616a7cba811c
DangubaGitHub <dangubicjovan@outlook.com>
2022-07-12 16:50:53 +0200

    added repeating background + fixed element positioning

---
+++ Tower Jump/Assets/Scripts/RepeatingFG.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingFG : MonoBehaviour
+{
+    [SerializeField] float scroleSpeed = -2f;
+    [SerializeField] float number;
+    Vector2 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * scroleSpeed, number);
+        transform.position = startPosition + Vector2.up * newPos;
+    }
+}
