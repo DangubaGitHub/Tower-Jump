f292b163ec245e9111ab9dabaf06a8102349efab
DangubaGitHub <dangubicjovan@outlook.com>
2022-07-13 21:26:14 +0200

    added platform spawner (finished)

--- Tower Jump/Assets/Scripts/Level/Platform.cs
+++ Tower Jump/Assets/Scripts/Level/Platform.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
+    [SerializeField] float fallSpeed;
+    Rigidbody2D platformRb2d;
+
+    void Awake()
+    {
+        platformRb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        platformRb2d.velocity = new Vector2(0, -fallSpeed);
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Platform Destroyer"))
---
+++ Tower Jump/Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+    [SerializeField] Transform[] spawnPoints;
+    [SerializeField] GameObject prefab;
+    
+    [SerializeField] float timeBetweenSpawn;
+    float nextSpawnTime;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Time.time > nextSpawnTime)
+        {
+            SpawnPlatform();
+
+            nextSpawnTime = Time.time + timeBetweenSpawn;
+        }
+    }
+
+    void SpawnPlatform()
+    {
+        int randSpawnPoint = Random.Range(0, spawnPoints.Length);
+
+        GameObject platform = Instantiate(prefab, spawnPoints[randSpawnPoint].position, transform.rotation);
+        //Rigidbody2D platformRb2d = platform.GetComponent<Rigidbody2D>();
+        //platformRb2d.AddForce(Vector2.down * speed, ForceMode2D.Impulse);
+        //platformRb2d.velocity = new Vector2(0, -speed);
+
+    }
+}
