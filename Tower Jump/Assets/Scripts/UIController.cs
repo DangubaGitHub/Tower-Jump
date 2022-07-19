d1754b08063c97426af375aec858be81350e72e3
DangubaGitHub <dangubicjovan@outlook.com>
2022-07-19 21:44:49 +0200

    added menu panel and buttons

--- Tower Jump/Assets/Scripts/UIController.cs
+++ Tower Jump/Assets/Scripts/UIController.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
     [SerializeField] Text floorCountText;
     [SerializeField] Text pickupCounterText;
+    [SerializeField] GameObject menuPanel;
+    [SerializeField] GameObject resetButton;
 
     void Start()
     {
-        
+        menuPanel.SetActive(true);
+        resetButton.SetActive(false);
     }
 
     void Update()
@@ -27,5 +31,20 @@ public class UIController : MonoBehaviour
     void UpdatePickupCount()
     {
         pickupCounterText.text = GameManager.instance.collectibleCount.ToString();
+    }
+
+    public void StartGame()
+    {
+        menuPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ResetScore()
+    {
+
     }
 }
